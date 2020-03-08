e6b1bdaef3af270dfc85f2001c3cb73381f96fa7
julio.cesar.oliveira <julio.cesar.oliveira@accenture.com>
2020-03-08 20:33:45 -0300

    Organização das pastas

---
+++ FrontEnd/VueNovoProjeto/vueproject/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <Alunos/>
+  </div>
+</template>
+
+<script>
+import Alunos from './components/Alunos.vue'
+
+export default {
+  name: 'app',
+  components: {
+    Alunos
+  }  
+}
+</script>
+
+<style>
+#app {
+
+}
+</style>
---
+++ FrontEnd/VueNovoProjeto/vueproject/src/components/Alunos.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <h1>{{nome}}</h1>
+    <input type="text" placeholder="Nome Aluno" v-model="nome" />
+    <button class="btn" @click="addAluno()">Add</button>
+    <table class="table">
+      <table class="table table-dark">
+        <thead>
+          <tr>
+            <th>Matrícula</th>
+            <th>Aluno</th>
+            <th>Ação</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="(aluno, index) in alunos" :key="index">
+            <td>a</td>
+            <td {{aluno.nome}}></td>
+            <td>b</td>
+          </tr>
+        </tbody>
+      </table>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      nome: "",
+      alunos: []
+    };
+  },
+  props: {},
+  methods: {
+    addAluno() {
+      console.log("oi");
+      let _aluno = { nome: this.nome };
+      this.alunos.push(_aluno);
+    }
+  }
+};
+</script>
+
+<style scoped>
+</style>
