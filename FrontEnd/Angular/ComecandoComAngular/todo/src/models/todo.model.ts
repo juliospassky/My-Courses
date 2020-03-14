af4ff1033de8e34dcfad1c02901c93558424eef9
julio.cesar.oliveira <julio.cesar.oliveira@accenture.com>
2020-03-14 16:12:06 -0300

    ComecandoComAngular - Finalização

---
+++ FrontEnd/Angular/ComecandoComAngular/todo/src/models/todo.model.ts
@@ -0,0 +1,13 @@
+export class Todo {
+
+    constructor(public id: Number = 1, public title: String, public done: Boolean) { }
+
+    remove(todo: Todo)
+    {
+        
+    }
+
+    markAsDone(){}
+
+    markAsUnDone(){}
+}
