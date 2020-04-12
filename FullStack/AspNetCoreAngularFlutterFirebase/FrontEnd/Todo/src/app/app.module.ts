d07de62ce9c15f5c3366237835e565d1aa2197d7
juliocsoft <juliocto2015@gmail.com>
2020-04-12 16:59:23 -0300

    :rocket: MVP front

---
+++ FullStack/AspNetCoreAngularFlutterFirebase/FrontEnd/Todo/src/app/app.module.ts
@@ -0,0 +1,40 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { CardComponent } from './components/card/card.component';
+import { ButtonComponent } from './components/button/button.component';
+import { LoginComponent } from './pages/login/login.component';
+import { UserCardComponent } from './components/user-card/user-card.component';
+import { TabsComponent } from './components/tabs/tabs.component';
+import { TodoListComponent } from './components/todo-list/todo-list.component';
+import { AllComponent } from './pages/all/all.component';
+import { TodayComponent } from './pages/today/today.component';
+import { TomorrowComponent } from './pages/tomorrow/tomorrow.component';
+import { NewComponent } from './pages/new/new.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    CardComponent,
+    ButtonComponent,
+    LoginComponent,
+    UserCardComponent,
+    TabsComponent,
+    TodoListComponent,
+    AllComponent,
+    TodayComponent,
+    TomorrowComponent,
+    NewComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
