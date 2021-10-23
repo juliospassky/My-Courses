6515303b8e1df9cfa67690fbdb442d03b0f3dc67
jo55658 <juliocto2015@gmail.com>
2021-10-23 11:12:55 -0300

    :tada: First commit

---
+++ BackEnd/DDD/NerdStore/src/NerdStore.Catalogo.Domain/Categoria.cs
@@ -0,0 +1,29 @@
+﻿using NerdStore.Core.DomainObjects;
+
+namespace NerdStore.Catalogo.Domain
+{
+    public class Categoria : Entity
+    {
+        public string Nome { get; set; }
+        public string Codigo { get; set; }
+
+        public Categoria(string nome, string codigo)
+        {
+            Nome = nome;
+            Codigo = codigo;
+
+            Validar();
+        }
+
+        public void Validar()
+        {
+            Validacoes.ValidarSeVazio(Nome, "O campo Nome da categoria não pode estar vazio");
+            Validacoes.ValidarSeIgual(Codigo, 0, "O campo Codigo da categoria não pode ser 0");
+        }
+
+        public override string ToString()
+        {
+            return $"{Nome} - {Codigo}";
+        }
+    }
+}
---
+++ BackEnd/DDD/NerdStore/src/NerdStore.Core/GlobalSuppressions.cs
@@ -0,0 +1,8 @@
+﻿// This file is used by Code Analysis to maintain SuppressMessage
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given
+// a specific target and scoped to a namespace, type, member, etc.
+
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("Blocker Code Smell", "S3875:\"operator==\" should not be overloaded on reference types", Justification = "<Pendente>", Scope = "member", Target = "~M:NerdStore.Core.DomainObjects.Entity.op_Equality(NerdStore.Core.DomainObjects.Entity,NerdStore.Core.DomainObjects.Entity)~System.Boolean")]
